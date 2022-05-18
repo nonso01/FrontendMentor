0c520cae2ef4bc16488fafee9e76a039b3a233cf
Martin <84927159+nonso01@users.noreply.github.com>
2022-05-18 18:31:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const body= document.getElementById('BODY');
+let myInfo={
+ 'dateCreated':'05/05/22',
+ 'challengedFrom': 'frontendmentor',
+ 'solvedBy':'nonso Martin',
+ 'used':['html','css','scss']
+};
+//body.style.transform='scale(0.5,0.5)';
